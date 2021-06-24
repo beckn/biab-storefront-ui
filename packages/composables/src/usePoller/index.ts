80b329a6831ec1c5ff79517f0d76293d6a7816f7
Chirag Katare <katare.chirag@autoninja.in>
2021-06-24 18:45:23 +0530

    added test cases

--- packages/composables/src/usePoller/index.ts
+++ packages/composables/src/usePoller/index.ts
@@ -1,13 +1,9 @@
-import { vsfRef, useVSFContext } from '@vue-storefront/core';
-import { Context } from '@vue-storefront/core';
+import { vsfRef, Context } from '@vue-storefront/core';
 
 const usePoller = () => {
   const pollResults = vsfRef([], 'poll-results');
-  const context = useVSFContext() as Context;
-
-  const poll = async (messageId) => {
-    console.log('In poller: ' + messageId);
-    pollResults.value.push(await context.$beckn.api.onSearch({messageId}));
+  const poll = async (context: Context, messageId) => {
+    pollResults.value.push(await context.$beckn.api.onSearch({ messageId }));
   };
 
   return { pollResults, poll };
