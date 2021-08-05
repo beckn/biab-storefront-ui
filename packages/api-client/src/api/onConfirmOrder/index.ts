993a24cb66490b5dc6b4563dd59ef5cce7f1d1ea
Chirag Katare <katare.chirag@autoninja.in>
2021-08-05 23:49:25 +0530

    rebase changes

--- packages/api-client/src/api/onConfirmOrder/index.ts
+++ packages/api-client/src/api/onConfirmOrder/index.ts
@@ -1,10 +1,9 @@
 import { Config } from './../../types/Setup';
 import * as sa from 'superagent';
-import { onInitiatizeOrderParam } from '../../types/InitiatizeOrder';
 import { AckResponse } from '../../types/BecknClientApi';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default async function onConfirmOrder(context, params: onInitiatizeOrderParam): Promise<AckResponse> {
+export default async function onConfirmOrder(context, params): Promise<AckResponse> {
   const config = (context.config as Config);
   const client = (context.client as sa.SuperAgent<sa.SuperAgentRequest>);
   return client.get(config.api.url + config.api.endpoints.onConfirmOrder)
