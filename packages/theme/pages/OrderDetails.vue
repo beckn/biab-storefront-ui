70bf9d7dd7adb2a2caacc8ad76501c3adb7c89a3
Ujjwal <shyamkumartiwari405@gmail.com>
2022-02-08 19:40:18 +0530

    Change :- using order_id instead of message_id for polling onOrderStatus

--- packages/theme/pages/OrderDetails.vue
+++ packages/theme/pages/OrderDetails.vue
@@ -521,7 +521,7 @@ export default {
 
       try {
         const response = await status(params);
-        await onStatus({ messageId: response.context.message_id });
+        await onStatus({ messageId: order.value.order.id });
       } catch (error) {
         console.log('Error calling status apis - ', error);
       }
